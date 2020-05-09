e1688c097b1d8eaf430a9b4129a5d4c7d5acd90d
Pooja B Shankar <nag.shankar6@gmail.com>
2020-05-09 15:53:37 +0530

    server

--- server/server.js
+++ server/server.js
@@ -7,6 +7,12 @@ const port = process.env.PORT || 3000; //use automatic PORT set by heroku if its
 
 app.use(express.static(publicPath)); //telling express to serve up public directory
 
+//if the requested file by the user is not present in public folder then give them back the index.html folder
+app.get('*', (req,res)=>{
+   res.sendFile(path.join(publicPath, 'index.html'));    
+})
+
+
 //start server for heroku or local running
 app.listen(port, ()=>{
     console.log('server is up')
